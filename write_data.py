a6e9a5fbcd6317a680a5942e8fdaff3c976eadb1
Magnus <magnus@bogh.dk>
2015-05-06 11:11:57 +0400

    added ability to eddit multiple cells

--- write_data.py
+++ write_data.py
@@ -9,5 +9,19 @@ scope = ['https://spreadsheets.google.com/feeds']
 credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
 gc = gspread.authorize(credentials)
 
-wks = gc.open("data_1").sheet1
-wks.update_acell('B9', "moe is cool")
+wks = gc.open("data_2").sheet1
+
+row = 2
+UID = 0000
+Sign_io = "True"
+Date = "12/12/15"
+Time = "23:22"
+FName = "Magnus"
+LName = "Christensen"
+
+wks.update_cell(row, 1, UID)
+wks.update_cell(row, 2, Sign_io)
+wks.update_cell(row, 3, Date)
+wks.update_cell(row, 4, Time)
+wks.update_cell(row, 5, FName)
+wks.update_cell(row, 6, LName)
