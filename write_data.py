78febdce771e1f167abed5571b70318a087a2e91
Magnus <magnus@bogh.dk>
2015-05-06 12:31:00 +0400

    added the ability to read and write to both spreedsheets, need to connect to RFID reader.

--- write_data.py
+++ write_data.py
@@ -2,6 +2,7 @@ __author__ = 'magnus'
 
 import json
 import gspread
+import time
 from oauth2client.client import SignedJwtAssertionCredentials
 
 json_key = json.load(open('ITGS-test-01071b9eba81.json'))
@@ -9,19 +10,38 @@ scope = ['https://spreadsheets.google.com/feeds']
 credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
 gc = gspread.authorize(credentials)
 
-wks = gc.open("data_2").sheet1
+wks1 = gc.open("data_1").sheet1
+wks2 = gc.open("data_2").sheet1
 
 row = 2
-UID = 0000
+CARD_ID = "F9122"
+UID = "0000"
 Sign_io = "True"
-Date = "12/12/15"
-Time = "23:22"
-FName = "Magnus"
-LName = "Christensen"
+Date = "error"
+Time = "error"
+FName = "error"
+LName = "error"
+check_value = 1
 
-wks.update_cell(row, 1, UID)
-wks.update_cell(row, 2, Sign_io)
-wks.update_cell(row, 3, Date)
-wks.update_cell(row, 4, Time)
-wks.update_cell(row, 5, FName)
-wks.update_cell(row, 6, LName)
+while True:
+
+    check = wks1.cell(check_value, 1).value
+    print check
+    if check == CARD_ID:
+        values_list = wks1.row_values (check_value)
+        values_list.pop(0)
+        values_list.insert(1, Sign_io)
+        values_list.insert(2, time.strftime("%d/%m/%Y"))
+        values_list.insert(3, time.strftime("%I:%M:%S"))
+        wks2.append_row(values_list)
+        break
+    elif not check:
+        print "Card ID does not exists"
+        break
+    check_value += 1
+
+
+
+
+#wks2.resize(1)
+
