224f9d2d1bc03f46405d0e63636887fd0993d635
Magnus <magnus@bogh.dk>
2015-05-11 18:14:12 +0400

    added local csv reading capability

--- write_data.py
+++ write_data.py
@@ -3,6 +3,7 @@ __author__ = 'magnus'
 import json
 import gspread
 import time
+import csv
 from oauth2client.client import SignedJwtAssertionCredentials
 
 json_key = json.load(open('ITGS-test-01071b9eba81.json'))
@@ -10,11 +11,12 @@ scope = ['https://spreadsheets.google.com/feeds']
 credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
 gc = gspread.authorize(credentials)
 
-wks1 = gc.open("data_1").sheet1
 wks2 = gc.open("data_2").sheet1
 
+
+
 row = 2
-CARD_ID = "F9122"
+CARD_ID = "F9322"
 UID = "0000"
 Sign_io = "True"
 Date = "error"
@@ -23,19 +25,28 @@ FName = "error"
 LName = "error"
 check_value = 1
 
-while True:
+with open('data_1.csv', 'rb') as f:
+        reader = csv.reader(f)
+        row_count = sum(1 for row in reader)
 
-    check = wks1.cell(check_value, 1).value
+while True:
+    with open('data_1.csv', 'rb') as f:
+        reader = csv.reader(f)
+        reader = list(reader)
+        check = reader[check_value][0]
     print check
     if check == CARD_ID:
-        values_list = wks1.row_values (check_value)
+        with open('data_1.csv', 'rb') as f:
+            reader = csv.reader(f)
+            for i in range(check_value+1):
+                values_list = reader.next()
         values_list.pop(0)
         values_list.insert(1, Sign_io)
         values_list.insert(2, time.strftime("%d/%m/%Y"))
         values_list.insert(3, time.strftime("%I:%M:%S"))
         wks2.append_row(values_list)
         break
-    elif not check:
+    elif check_value == row_count-1:
         print "Card ID does not exists"
         break
     check_value += 1
