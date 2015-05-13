d1f08c61a0a785bcbd79e56180cbffb1b6769224
magnusbbc <magnus@bogh.dk>
2015-05-14 01:00:33 +0400

    Finished most code. TODO, ability to delete user, add user with rfid tag, write log to a local buffer file

--- Pi example_1.py
+++ Pi example_1.py
@@ -32,10 +32,17 @@ with open('data_1.csv', 'rb') as f:
 #setup var for RFID
 continue_reading = True
 
+#Setup for output
+GPIO.setwarnings(False)
+GPIO.cleanup()
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT) #GREEN LED
+GPIO.setup(13, GPIO.OUT) #RED LED
+GPIO.setup(15, GPIO.OUT) #Buzzer
+
 # Capture SIGINT for cleanup when the script is aborted
 def end_read(signal,frame):
     global continue_reading
-    print "Ctrl+C captured, ending read."
     continue_reading = False
     GPIO.cleanup()
 
@@ -47,10 +54,11 @@ MIFAREReader = MFRC522.MFRC522()
 print "Ctrl+C to stop capture"
 wks2 = gc.open("data_2").sheet1
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
-
+print ("Welcome to GEMS AMERICAN ACADEMY RFID Staff sign in/out system!\n")
 while True:
-    input_var = input("0.Initiate Reading\n1.Add User\n2.Delete User\nEnter Value:")
+    input_var = input("0.Initiate Reading\n1.Add User\n2.Delete User\n3.End Program\n4.Documentation\n5.About\nEnter Value:")
     if input_var == 0:
+        print "Ctrl+C to stop capture"
         while continue_reading:
             # Scan for cards
             (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
@@ -78,6 +86,8 @@ while True:
                         check = reader[check_value][0]
                     print check
                     if check == UID_check:
+                        GPIO.output(11, True)
+                        GPIO.output(15, True)
                         with open('data_1.csv', 'rb') as f:
                             reader = csv.reader(f)
                             for i in range(check_value+1):
@@ -87,12 +97,19 @@ while True:
                         values_list.insert(2, time.strftime("%d/%m/%Y"))
                         values_list.insert(3, time.strftime("%I:%M:%S"))
                         wks2.append_row(values_list)
+                        time.sleep(1)
+                        GPIO.output(11, False)
+                        GPIO.output(15, False)
                         loop = 0
                     elif check_value == row_count-1:
                         print "Card ID does not exists"
+                        GPIO.output(13, True)
+                        time.sleep(1)
+                        GPIO.output(13, False)
                         loop = 0
                     check_value += 1
     elif input_var == 1:
+        #TODO: READ CARD ID
         input_list[0] = raw_input("Enter Card number:")
         input_list[1] = 1000+(row_count - 1)
         input_list[2] = raw_input("Enter First Name:")
@@ -103,5 +120,14 @@ while True:
     elif input_var == 2:
         #TODO
         print ("todo")
+    elif input_var == 3:
+        break
+    elif input_var == 4:
+        #TODO
+        print("TODO")
+    elif input_var == 5:
+        print ("RFAA\nMade by Magnus Bogh Borregaard Christensen\nVersion: 0.7")
+
     else:
-        print ("Incorrect Value, must be between 0 and 2!")
+        print ("Incorrect Value, must be between 0 and 3!")
+GPIO.cleanup()
